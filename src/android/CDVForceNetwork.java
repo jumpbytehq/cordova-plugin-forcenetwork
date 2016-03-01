1e04d0f08a11fbdf4e9bd8577b9973ce2f022b49
Dhaval Nagar <dhaval.b.nagar@gmail.com>
2016-03-01 20:01:16 +0530

    Android fix

--- src/android/CDVForceNetwork.java
+++ src/android/CDVForceNetwork.java
@@ -1,1 +1,43 @@
-CDVForceNetwork.java
+
+package org.apache.cordova.device;
+
+import java.util.TimeZone;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaInterface;
+import android.provider.Settings;
+
+public class CDVForceNetwork extends CordovaPlugin {
+    public static final String TAG = "CDVForceNetwork";
+    
+    /**
+     * Constructor.
+     */
+    public CDVForceNetwork() {
+    }
+
+    /**
+     * Executes the request and returns PluginResult.
+     *
+     * @param action            The action to execute.
+     * @param args              JSONArry of arguments for the plugin.
+     * @param callbackContext   The callback id used when calling back into JavaScript.
+     * @return                  True if the action was valid, false if not.
+     */
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if ("openNetworkSettings".equals(action)) {
+            Context context=this.cordova.getActivity().getApplicationContext(); 
+            context.startActivity(new Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS));            
+        }
+        else if("enableWifi".equals(action)){
+            Context context=this.cordova.getActivity().getApplicationContext(); 
+            WifiManager wifiManager = (WifiManager)context.getSystemService(context.WIFI_SERVICE);
+            wifiManager.setWifiEnabled(true);
+        }
+        else {
+            return false;
+        }
+        return true;
+    }
+}
